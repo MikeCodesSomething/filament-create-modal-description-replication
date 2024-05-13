c14e695f251c105d5b98bd1ff00e8d074c7df718
Rory Lockett <rory.lockett@northernestateagencies.co.uk>
2024-05-13 11:41:33 +0100

    initial commit

---
+++ app/Filament/Resources/SheetResource.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SheetResource\Pages;
+use App\Filament\Resources\SheetResource\RelationManagers;
+use App\Models\Sheet;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SheetResource extends Resource
+{
+    protected static ?string $model = Sheet::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name'),
+                FileUpload::make('sheet')
+                    ->disk(config('filesystems.default'))
+                    ->acceptedFileTypes(['text/csv'])
+                    ->downloadable(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSheets::route('/'),
+            'create' => Pages\CreateSheet::route('/create'),
+            'edit' => Pages\EditSheet::route('/{record}/edit'),
+        ];
+    }
+}
